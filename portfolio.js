24673be446e1a2fbb07224ce91d02d1154702038
Uriel Azar <uriel.azar01@gmail.com>
2019-08-06 17:53:41 +0300

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -21,4 +21,11 @@ $(document).ready(function () {
             });
         } // End if
     });
+
+    //fix scroll on safari 
+    var deviceAgent = navigator.userAgent.toLowerCase();
+    var agentID = deviceAgent.match(/(iPad|iPhone|iPod)/i);
+    if (agentID) {
+        $('body').css('-webkit-overflow-scrolling', 'touch');
+    }
 });
