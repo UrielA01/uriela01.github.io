6673280d3cdbc6f39e4f7feb02f5d5c245f50c72
UrielA01 <uriel.azar01@gmail.com>
2023-11-06 23:27:11 +0200

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Body from './components/Body';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
     </div>
   );
 }
